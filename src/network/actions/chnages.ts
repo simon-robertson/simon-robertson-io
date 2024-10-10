349c391839c8d06832950906bcb2dc583c39b69f
Simon Robertson <hello@simon-robertson.io>
2024-10-10 10:02:25 +0100

    Update the change log

--- src/network/actions/chnages.ts
+++ src/network/actions/chnages.ts
@@ -7,6 +7,10 @@ export async function fetchChanges(): Promise<ChangeInfo[]> {
     // TODO: Move these over to Turso
     return [
         {
+            date: "2024-08-10",
+            description: "Improved the UI.",
+        },
+        {
             date: "2024-08-08",
             description: "Switched the website over to NextJS in preparation for planned features.",
         },
