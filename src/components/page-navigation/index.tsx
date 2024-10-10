3f242851ce452f350fbdd6aedc3de60544325076
Simon Robertson <hello@simon-robertson.io>
2024-10-10 10:01:12 +0100

    Make page navigation links dynamic

--- src/components/page-navigation/index.tsx
+++ src/components/page-navigation/index.tsx
@@ -1,11 +1,19 @@
+import { getBaseRoutes } from "@/routes"
+
 export function PageNavigation() {
+    const routes = getBaseRoutes()
+
+    const linkNodes = routes.map((route) => {
+        return (
+            <a key={route.path} href={route.path}>
+                {route.title}
+            </a>
+        )
+    })
+
     return (
         <nav className="row">
-            <div className="col-mid page-navigation">
-                <a href="/">Home</a>
-                {/* <a href="/articles">Articles</a> */}
-                {/* <a href="/playground">Playground</a> */}
-            </div>
+            <div className="col-mid page-navigation">{linkNodes}</div>
         </nav>
     )
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 type Route = {
     readonly path: string
     readonly title: string
+    readonly parent: string
     readonly description: string
 }
 
@@ -10,6 +11,7 @@ export const routes: Route[] = [
     {
         path: "/",
         title: "Simon Robertson",
+        parent: "",
         description:
             "A creator of web-based software, with a passion for creating interesting or innovative web-based applications.",
     },
@@ -28,4 +30,10 @@ export function getMetadataForPath(path: string): Metadata {
         title: route.title,
         description: route.description,
     }
+}
+
+export function getBaseRoutes(): Route[] {
+    return routes.filter((route) => {
+        return route.parent === ""
+    })
 }
