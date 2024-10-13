f1025be621ffd1e3b427e5d2b40b36003b505858
Simon Robertson <hello@simon-robertson.io>
2024-10-13 21:13:50 +0100

    Add a helper function to convert simple markdown

---
+++ src/helpers/markdown.ts
@@ -0,0 +1,105 @@
+export async function parseMarkdown(contents: string): Promise<string> {
+    const time = Date.now()
+
+    const source: string[] = contents.trim().split(/\r?\n/)
+    const output: string[] = []
+
+    let sourceIndex = 0
+    let outputIndex = 0
+
+    let capturingCode = false
+    let capturingList = false
+
+    while (sourceIndex < source.length) {
+        let line = source[sourceIndex++].trimEnd()
+
+        if (capturingCode) {
+            if (/^\s*```/.test(line)) {
+                output[outputIndex++] = "</pre>"
+                output[outputIndex++] = "</div>"
+                capturingCode = false
+                continue
+            }
+
+            output[outputIndex++] = "<code>"
+
+            if (line === "") {
+                output[outputIndex++] = " "
+            } else {
+                output[outputIndex++] = line
+            }
+
+            output[outputIndex++] = "</code>"
+            continue
+        }
+
+        if (/\[.+?\]\(.+?\)/.test(line)) {
+            line = line.replaceAll(/\[(.+?)\]\((.+?)\)/g, (...args: string[]) => {
+                return `<a href="${args[2]}">${args[1]}</a>`
+            })
+        }
+
+        if (/\*\*.+?\*\*/.test(line)) {
+            line = line.replaceAll(/\*\*(.+?)\*\*/g, (...args: string[]) => {
+                return `<strong>${args[1]}</strong>`
+            })
+        }
+
+        if (/\*(.+?)\*/.test(line)) {
+            line = line.replaceAll(/\*(.+?)\*/g, (...args: string[]) => {
+                return `<em>${args[1]}</em>`
+            })
+        }
+
+        if (capturingList) {
+            if (/^\s*\+/.test(line) === false) {
+                output[outputIndex++] = "</ul>"
+                capturingList = false
+                continue
+            }
+
+            output[outputIndex++] = "<li>"
+            output[outputIndex++] = line.trimStart()
+            output[outputIndex++] = "</li>"
+            continue
+        }
+
+        if (line === "") {
+            continue
+        }
+
+        if (/^\s*```/.test(line)) {
+            output[outputIndex++] = '<div class="code-block">'
+            output[outputIndex++] = "<pre>"
+            capturingCode = true
+            continue
+        }
+
+        if (/^\s*\+/.test(line)) {
+            output[outputIndex++] = "<ul>"
+            capturingList = true
+            continue
+        }
+
+        if (/^\s*#/.test(line)) {
+            const matches = line.match(/^\s*(#+)\s*(.+)/)!
+
+            output[outputIndex++] = "<h"
+            output[outputIndex++] = matches[1].length.toString()
+            output[outputIndex++] = ">"
+            output[outputIndex++] = matches[2]
+            output[outputIndex++] = "</h"
+            output[outputIndex++] = matches[1].length.toString()
+            output[outputIndex++] = ">"
+            continue
+        }
+
+        output[outputIndex++] = "<p>"
+        output[outputIndex++] = line.trimStart()
+        output[outputIndex++] = "</p>"
+    }
+
+    console.log("Processing time:", Date.now() - time)
+
+    return output.join("")
+}
